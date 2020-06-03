47e8a45f5ec0315d63aeb089dc58a04b14356275
William Cao <wcao00@stuy.edu>
2020-06-03 13:06:46 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Test(unittest.TestCase):
+  def test_one(self):
+    assertEqual("asd", "asd")
+  
+  def test_fail(self):
+    assertEqual("not equal", "equal")
+
+if __name__ == "__main__":
+  unittest.main()
