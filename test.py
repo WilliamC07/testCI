2275090d8456de0e245d2ddab073c493a7027cb4
William Cao <wcao00@stuy.edu>
2020-06-03 13:12:20 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import unittest
 
 class Test(unittest.TestCase):
   def test_one(self):
-    assertEqual("asd", "asd")
+    self.assertEqual("asd", "asd")
   
   def test_fail(self):
-    assertEqual("not equal", "equal")
+    self.assertEqual("not equal", "equal")
 
 if __name__ == "__main__":
   unittest.main()
